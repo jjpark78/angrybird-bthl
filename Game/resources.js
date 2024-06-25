6cc211746ab752984b4aa87f22ab4d4627616ec3
jungkun <yonghun.jeong@friacode.com>
2024-06-26 00:27:51 +0900

    add party gif image and load image code

--- Game/resources.js
+++ Game/resources.js
@@ -1,13 +1,31 @@
 var g_ressources = (function () {
-    var retval = [],
-        imgs = ["bg", "platform", "bird", "enemy", "sling1", "sling2", "sling3", "ground", "wood1", "wood2", "smoke", "menu_refresh", "menu_back"];
+  var retval = [],
+    imgs = [
+      "bg",
+      "platform",
+      "bird",
+      "enemy",
+      "sling1",
+      "sling2",
+      "sling3",
+      "ground",
+      "wood1",
+      "wood2",
+      "smoke",
+      "menu_refresh",
+      "menu_back",
+      "party",
+    ];
 
-    for (var i = 0; i < imgs.length; i++) {
-        retval.push({
-            type: "image",
-            src: 'sprites/' + imgs[i] + '.png'
-        });
-    }
+  for (var i = 0; i < imgs.length; i++) {
+    // image file extension by jungkun
+    var ext = imgs[i] === "party" ? ".gif" : ".png";
+    retval.push({
+      type: "image",
+      // src: "sprites/" + imgs[i] + ".png",
+      src: "sprites/" + imgs[i] + ext,
+    });
+  }
 
-    return retval;
-}());
+  return retval;
+})();
