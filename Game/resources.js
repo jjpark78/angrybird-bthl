57197ce9dd245f06db0774d48444fe691813a410
jungkun <yonghun.jeong@friacode.com>
2024-06-28 16:55:35 +0900

    add retry scene

--- Game/resources.js
+++ Game/resources.js
@@ -1,51 +1,52 @@
 var bgm_sound = "sounds/bgm.mp3",
-	effect_sound = "sounds/effect.mp3",
-	impact_sound = "sounds/impact.mp3",
-    fail_sound = "sounds/fail.mp3";
+  effect_sound = "sounds/effect.mp3",
+  impact_sound = "sounds/impact.mp3",
+  fail_sound = "sounds/fail.mp3";
 
 var result_arr = [];
 
 var g_ressources = (function () {
-    var retval = [],
-        imgs = [
-            "intro01",
-            "intro2",
-            "intro3",
-            "intro4",
-            "bg",
-            "bg_finish",
-            "platform", 
-            "bird", 
-            "poll", 
-            "sling1", 
-            "sling2", 
-            "sling3", 
-            "ground", 
-            "wood1", 
-            "wood2", 
-            "smoke", 
-            "menu_refresh", 
-            "menu_back",
-            "goal01",
-            "goal02",
-            "goal03",
-            "goal04",
-            "goal05",
-        ],
-        sounds = [bgm_sound, effect_sound, impact_sound];
+  var retval = [],
+    imgs = [
+      "intro01",
+      "intro2",
+      "intro3",
+      "intro4",
+      "bg",
+      "bg_finish",
+      "platform",
+      "bird",
+      "poll",
+      "sling1",
+      "sling2",
+      "sling3",
+      "ground",
+      "wood1",
+      "wood2",
+      "smoke",
+      "menu_refresh",
+      "menu_back",
+      "goal01",
+      "goal02",
+      "goal03",
+      "goal04",
+      "goal05",
+      "retry",
+    ],
+    sounds = [bgm_sound, effect_sound, impact_sound];
 
-    for (var i = 0; i < sounds.length; i++) {
-        retval.push({
-            type: "sound",
-            src: sounds[i]
-        });
-    }
-    for (var i = 0; i < imgs.length; i++) {
-        retval.push({
-            type: "image",
-            src: 'sprites/' + imgs[i] + '.png'
-        });
-    }
+  for (var i = 0; i < sounds.length; i++) {
+    retval.push({
+      type: "sound",
+      src: sounds[i],
+    });
+  }
+  for (var i = 0; i < imgs.length; i++) {
+    retval.push({
+      type: "image",
+      src: "sprites/" + imgs[i] + ".png",
+    });
+  }
 
-    return retval;
-}());
+  return retval;
+})();
--- Game/retry.js
+++ Game/retry.js
@@ -1,72 +1,71 @@
 var RetryLayer = cc.Layer.extend({
-    menus: [],
-    getTexture: function (name) {
-        return cc.TextureCache.getInstance()
-            .addImage('sprites/' + name + '.png');
-    },
-    init: function () {
-        this._super();
-        this.removeAllChildrenWithCleanup(true);
-        this.setTouchEnabled(true);
+  menus: [],
+  getTexture: function (name) {
+    return cc.TextureCache.getInstance().addImage("sprites/" + name + ".png");
+  },
+  init: function () {
+    this._super();
+    this.removeAllChildrenWithCleanup(true);
+    this.setTouchEnabled(true);
 
-        var director = cc.Director.getInstance(),
-            self = this,
-            winSize = director.getWinSize();
+    var director = cc.Director.getInstance(),
+      self = this,
+      winSize = director.getWinSize();
 
-        b2.initWorld();
+    b2.initWorld();
 
-        var scoreLabel = cc.LabelTTF.create("Retry", "fantasy", 50, cc.size(0, 0), cc.TEXT_ALIGNMENT_CENTER);
-        scoreLabel.setPosition(cc.p(winSize.width/2+ 60, winSize.height/2 + 100));
-        scoreLabel.setColor(cc.c3b(0, 0, 0));
-        this.addChild(scoreLabel, 5);
+    var RetryBGItem = cc.MenuItemImage.create(
+      "sprites/retry.png",
+      "sprites/retry.png",
+      this.onMainMenuCallback,
+      this
+    );
+    RetryBGItem.setScale(1.8);
+    RetryBGItem.setPosition(0, 0);
+    imageMenu = cc.Menu.create(RetryBGItem);
+    this.addChild(imageMenu, 1);
 
-        var menuItem = cc.MenuItemImage.create("sprites/menu_refresh.png","sprites/menu_refresh.png", this.onMainMenuCallback, this);
-		var imageMenu = cc.Menu.create(menuItem);
-        menuItem.setPosition(0,0);
-		this.addChild(imageMenu, 2);
+    this.scheduleUpdate();
+  },
+  onMainMenuCallback: function () {
+    var director = cc.Director.getInstance();
+    var scene = cc.Scene.create();
+    var layer = new GameScene();
+    scene.addChild(layer);
+    result_arr = [];
 
-        this.scheduleUpdate();
-    },
-	onMainMenuCallback: function () {
-        var director = cc.Director.getInstance();
-        var scene = cc.Scene.create();
-        var layer = new GameScene();
-        scene.addChild(layer);
-        result_arr = [];
-
-        var transition = cc.TransitionFade.create(0.5,scene);
-        director.replaceScene(transition);
-	},
-    update: function (dt) {
-    },
-    onTouchesBegan: function (touch, evt) {
-        this.menus.forEach(function (menu) {
-            menu.handleTouches(touch, evt);
-        });
-    },
-    onTouchesMoved: function (touch, evt) {
-        this.menus.forEach(function (menu) {
-            menu.handleTouchesMoved(touch, evt);
-        });
-    },
-    onTouchesEnded: function (touch, evt) {
-        this.menus.forEach(function (menu) {
-            menu.handleTouchesEnded(touch, evt);
-        });
-    },
-    onKeyUp: function (e) {},
-    onKeyDown: function (e) {}
+    var transition = cc.TransitionFade.create(0.5, scene);
+    director.replaceScene(transition);
+  },
+  update: function (dt) {},
+  onTouchesBegan: function (touch, evt) {
+    this.menus.forEach(function (menu) {
+      menu.handleTouches(touch, evt);
+    });
+  },
+  onTouchesMoved: function (touch, evt) {
+    this.menus.forEach(function (menu) {
+      menu.handleTouchesMoved(touch, evt);
+    });
+  },
+  onTouchesEnded: function (touch, evt) {
+    this.menus.forEach(function (menu) {
+      menu.handleTouchesEnded(touch, evt);
+    });
+  },
+  onKeyUp: function (e) {},
+  onKeyDown: function (e) {},
 });
 
 //--------------------- Scene ---------------------
 
 var RetryScene = cc.Scene.extend({
-    onEnter: function () {
-        this._super();
+  onEnter: function () {
+    this._super();
 
-        var layer = new RetryLayer();
-        layer.init();
+    var layer = new RetryLayer();
+    layer.init();
 
-        this.addChild(layer);
-    }
+    this.addChild(layer);
+  },
 });
